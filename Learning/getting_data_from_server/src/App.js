50a58558c246719a5ff39f24702dbd494ceabfc6
Arin Arjani <arinarjani@gmail.com>
2021-04-06 16:57:30 -0700

    finished part 2 up to Promises and Errors

--- Learning/getting_data_from_server/src/App.js
+++ Learning/getting_data_from_server/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Note from './components/Note.js';
 import axios from 'axios';
+import noteServices from './services/notes.js';
 
 
 const App = (props) => {
@@ -8,38 +9,88 @@ const App = (props) => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(false)
 
+  /**
+   * Gets all the notes from the server: http://localhost:3001/notes
+   */
   const hook = () => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+    noteServices
+      .getAll()
+      .then(initalNotes => {
+        console.log('promise fulfilled');
+        setNotes(initalNotes)
       })
+      .catch(error => console.log(error))
   }
   
   useEffect(hook, [])
 
-  console.log('render', notes.length, 'notes')
+  console.log('render', notes.length, 'notes');
 
+  /**
+   * Takes the new note added by the user and updates both the server
+   * and the state with that new note
+   * 
+   * @param {Object} event The event object created by user input
+   */
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() > 0.5,
-      id: notes.length + 1,
-    }
+    };
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    noteServices
+      .create(noteObject)
+      .then(returnedNote => {
+        console.log('note added to db:', returnedNote);
+        setNotes(notes.concat(returnedNote));
+        setNewNote('');
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 
+  /**
+   * Updates newNote state with user input of a new note
+   * 
+   * @param {Object} event The event object created by user input
+   */
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
+  /**
+   * Toggles between true/false for each note based on if the user clicks
+   * the toggle button
+   * 
+   * @param {Number} id The event object created by user input
+   */
+  const toggleImportanceOf =(id) => {
+    console.log('note id', id)
+    // grab the note with the id of id
+    const note = notes.find(note => note.id === id);
+    // https://www.freecodecamp.org/news/javascript-object-destructuring-spread-operator-rest-parameter/#update-properties
+    const changedNote = {...note, important: !note.important}
+
+    noteServices
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => {
+          return note.id !== id ? note : returnedNote
+        }));
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  /**
+   * Either shows all notes if showAll is true or shows notes that
+   * have their important value to true
+   */
   const notesToShow = showAll
   ? notes
   : notes.filter(note => note.important)
@@ -54,7 +105,11 @@ const App = (props) => {
       </div>   
       <ul>
         {notesToShow.map(note => 
-            <Note key={note.id} note={note} />
+            <Note 
+              key={note.id} 
+              note={note} 
+              toggleImportance={() => toggleImportanceOf(note.id)} 
+            />
         )}
       </ul>
       <form onSubmit={addNote}>
---
+++ Learning/getting_data_from_server/src/services/notes.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+const baseURL = 'http://localhost:3001/notes';
+
+/**
+ * Gets all the notes from the baseURL
+ * 
+ * @returns {Promise} all the notes on the server
+ */
+const getAll = () => {
+    const request = axios.get(baseURL);
+    return request.then(response => response.data);
+};
+
+/**
+ * Takes a new note created by the user and updates the 
+ * notes data on baseURL
+ * 
+ * @returns {Promise} new note created
+ */
+const create = (newObject) => {
+    const request = axios.post(baseURL, newObject);
+    return request.then(response => response.data);
+};
+
+/**
+ * Allows the user to update an individual note's important property
+ * by making important equal true or false
+ * 
+ * @returns {Promise} new note updated
+ */
+const update = (id, newObject) => {
+    const request = axios.put(`${baseURL}/${id}`, newObject);
+    return request.then(response => {
+        console.log('update response',response); 
+        return response.data;
+    });
+};
+
+export default { getAll, create, update }
