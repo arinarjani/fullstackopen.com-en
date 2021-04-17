2a240a26d33356076cf414d0d2d3ec73794084b9
Arin Arjani <arinarjani@gmail.com>
2021-04-16 21:02:40 -0700

    still need to work on 3.17 - how to use redirect to PUT in express

--- Learning/simple_web_server/index.js
+++ Learning/simple_web_server/index.js
@@ -4,9 +4,10 @@ const cors = require('cors');
 const app = express();
 const Note = require('./models/note');
 
+app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
-app.use(express.static('build'));
+
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -28,17 +29,42 @@ app.get('/api/notes', (req, res) => {
     })
 });
 
-app.get('/api/notes/:id', (req, res) => {
-  Note.findById(req.params.id).then(foundNote => {
-    res.json(foundNote);
-  });
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+      .then(foundNote => {
+        if (foundNote) {
+          res.json(foundNote);
+        } else {
+          res.status(404).end();
+        }
+        
+      })
+      .catch(error => next(error));
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-  // const id = Number(req.params.id);
-  // notes = notes.filter(note => note.id !== id);
-  // console.log('notes is now', notes);
-  // res.status(204).end()
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id).then(result => {
+    res.status(204).end()
+  }).catch(error => {
+    next(error)
+  })
+});
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body;
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+       .then(updatedNote => {
+         res.json(updatedNote);
+       })
+       .catch(error => {
+         next(error);
+       })
 });
 
 app.post('/api/notes', (req, res) => {
@@ -65,7 +91,19 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } 
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -51,35 +51,59 @@ app.get('/api/persons', (req, res) => {
 });
 
 /** 3.2 */
-app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`);
+app.get('/info', (req, res, next) => {
+    Person.find({})
+          .then(allPeople => {
+            res.send(`<p>Phonebook has info for ${allPeople.length} people</p> <p>${new Date()}</p>`);
+          })
+          .catch(error => next(error))
 });
 
-/** 3.3 */
+/** 3.3, 3.18 */
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const person = persons.find(person => person.id === id);
-    console.log('person', person)
+    // const id = Number(req.params.id);
+    // const person = persons.find(person => person.id === id);
+    // console.log('person', person)
 
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).json({
-            error: `no person with that id: ${id}`
-        })
-    }
+    // if (person) {
+    //     res.json(person)
+    // } else {
+    //     res.status(404).json({
+    //         error: `no person with that id: ${id}`
+    //     })
+    // }
+
+    Person.findById(req.params.id)
+          .then(foundPerson => {
+              if (foundPerson) {
+                  res.json(foundPerson);
+              } else {
+                  res.status(404).end();
+              }
+          })
+          .catch(error => {
+              console.log(`no person with that id: ${req.params.id}`)
+          })
 });
 
-/** 3.4 */
-app.delete('/api/persons/:id', (req, res) => {
-    console.log('hit the delete route');
-    const id = Number(req.params.id);
-    persons = persons.filter(person => person.id !== id);
-    console.log(persons);
-    res.status(204).end();
+/** 3.4, 3.15 */
+app.delete('/api/persons/:id', (req, res, next) => {
+    // console.log('hit the delete route');
+    // const id = Number(req.params.id);
+    // persons = persons.filter(person => person.id !== id);
+    // console.log(persons);
+    // res.status(204).end();
+
+    Person.findByIdAndDelete(req.params.id)
+          .then(deletedPerson => {
+              res.status(204).end();
+          })
+          .catch(error => {
+              next(error);
+          })
 });
 
-/** 3.5, 3.14 */
+/** 3.5, 3.14, 3.17 */
 app.post('/api/persons', (req, res) => {
     const body = req.body;
 
@@ -104,6 +128,37 @@ app.post('/api/persons', (req, res) => {
         res.json(savedPerson);
     })
 });
+
+/** 3.17 */
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body;
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+          .then(updatedPerson => {
+              res.json(updatedPerson);
+          })
+          .catch(error => {
+              next(error);
+          })
+});
+
+/** 3.16 */
+const handleError = (error, req, res, next) => {
+    console.log(error.message);
+
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' });
+    }
+    
+    next(error);
+}
+
+app.use(handleError);
 
 const PORT = process.env.PORT;
 
