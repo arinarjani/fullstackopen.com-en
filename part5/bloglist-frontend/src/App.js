591366f9589c3b99ec2e3945e0ef427b2267831c
arin a <arinarjani@gmail.com>
2022-09-11 20:10:09 -0700

    working on part 5.3, but the setUser is not really setting the user for some reason in handleLogin in App.js

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -41,24 +41,10 @@ const App = () => {
     // )  
   }, [])
 
-<<<<<<< HEAD
-  // 5.2 - make the login permanent with local storage
-  useEffect(() => {
-    // third attempt
-    try {
-      setUser(JSON.parse(localStorage.loggedinBlogUser));
-      // blogService.setToken(user.token);
-    } catch (error) {
-      console.log(error)
-    }
-
-    // first attempt
-=======
 
   // 5.2 - make the login permanent
   useEffect(() => {
     // FIRST ATTEMPT
->>>>>>> 52855bb09e4a1eeeab55c545f55630c25783f85d
     // const user = JSON.parse(localStorage.loggedinBlogUser) || null;
 
     // if (user) {
@@ -67,11 +53,7 @@ const App = () => {
     // }
 
 
-<<<<<<< HEAD
-    // second attempt
-=======
     // SECOND ATTEMPT
->>>>>>> 52855bb09e4a1eeeab55c545f55630c25783f85d
     // let unmounted = false;
     // const userFromLocalStorage = JSON.parse(localStorage.loggedinBlogUser)
 
@@ -124,24 +106,6 @@ const App = () => {
     }
   }
 
-<<<<<<< HEAD
-  // 5.2 - handle logout
-  // TODO: maybe redirect to the login page, but HOW? 
-  //       for now you have to refresh the page to login again
-  const handleLogout = () => {
-    setUser(null);
-    localStorage.clear();
-  }
-
-  // 5.3 handle a way to add a new blog
-  const handleNewBlog = (e) => {
-    e.preventDefault();
-
-    // use blogServices to add a new blog
-    blogService.create(newBlog);
-  }
-
-=======
   // 5.2 - create a way to logout
   const handleLogout = () => {
     // set the user back to null
@@ -151,7 +115,6 @@ const App = () => {
   }
 
   // 5.1 make it so the user can login and only show the login form if the user is not logged in
->>>>>>> 52855bb09e4a1eeeab55c545f55630c25783f85d
   return (
     <div>
       {user === null
@@ -170,7 +133,6 @@ const App = () => {
             {blogs.map(blog =>
               <Blog key={blog.id} blog={blog} />
             )}
-<<<<<<< HEAD
             <AddBlog 
               handleNewBlog={handleNewBlog} 
               newBlog={newBlog}
@@ -179,9 +141,6 @@ const App = () => {
             <Logout
               logOut={handleLogout}
             />
-=======
-            <Logout handleLogout={handleLogout} />
->>>>>>> 52855bb09e4a1eeeab55c545f55630c25783f85d
           </>
       }
     </div>
