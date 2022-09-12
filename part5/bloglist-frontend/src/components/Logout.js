810a5bccc089ac8c3ec1e10bcdf8cdc5f2d88e16
arin a <arinarjani@gmail.com>
2022-09-11 20:13:55 -0700

    more merge changes needing to accept

--- part5/bloglist-frontend/src/components/Logout.js
+++ part5/bloglist-frontend/src/components/Logout.js
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
 import React from 'react';
 
 const Logout = ( {logOut} ) => {
     return (
         <button onClick={logOut}>logout</button>
-=======
-import React from 'react'
-
-const Logout = ( {handleLogout} ) => {
-    return (
-        <>
-            <button onClick={handleLogout}>logOut</button>
-        </>
->>>>>>> 52855bb09e4a1eeeab55c545f55630c25783f85d
     )
 }
 
